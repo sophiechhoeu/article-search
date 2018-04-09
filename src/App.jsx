87da9e6d7ccd73ab10a867132ac067a098c55f76
sophie.chhoeu <sophie.chhoeu@carsguide.com.au>
2018-04-09 23:32:55 +1000

    updated query syntax

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ class App extends Component {
   search() {
     const BASE_URL = 'https://api.nytimes.com/svc/search/v2/articlesearch.json?';
     const API_KEY = 'e65c285a3650428aa955f98e68c8f641'
-    const FETCH_URL = BASE_URL + 'q=' + this.state.query + '&api-key='+ API_KEY
+    const FETCH_URL = `${BASE_URL}q=${this.state.query}&api-key=${API_KEY}`
     console.log('FETCH_URL', FETCH_URL);
   }
 
